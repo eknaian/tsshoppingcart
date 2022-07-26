2f61ddf6bade8e92c25775a72e2c0b06cc442315
eknaian <eknaian@gmail.com>
2022-07-26 13:12:10 -0400

    finished Cart with logic

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,13 @@
-import StoreItems from "../data/items.json";
 import { Col, Row } from "react-bootstrap";
 import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 export function Store() {
   return (
     <>
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {StoreItems.map((item) => (
+        {storeItems.map((item) => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
